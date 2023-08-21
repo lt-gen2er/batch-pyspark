ef8027765df5d686ccec8a14cb0658bec22bc12b
Tong Liu <ltltmine@gmail.com>
2023-08-21 14:58:13 -0500

    update test

--- tests/functions/functions.py
+++ tests/functions/functions.py
@@ -1,5 +1,5 @@
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import col, sha2, concat_ws
+from pyspark.sql.functions import sha2, concat_ws
 
 def calculate_hash_code(df: DataFrame):
     # Concatenate all columns into a single string
@@ -10,7 +10,7 @@ def calculate_hash_code(df: DataFrame):
     return df.withColumn("hash_code", hash_col)
 
 def compare_data_content(df1, df2):
-    hash_codes_df1 = calculate_hash_code(df1).collect()
-    hash_codes_df2 = calculate_hash_code(df2).collect()
+    hash_codes_df1 = calculate_hash_code(df1).select("hash_code").rdd.flatMap(lambda x: x).collect()
+    hash_codes_df2 = calculate_hash_code(df2).select("hash_code").rdd.flatMap(lambda x: x).collect()
 
     return set(hash_codes_df1) == set(hash_codes_df2)
