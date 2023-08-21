577cbb7f5877fa49c028a759a38d2ea1463f6c54
Tong Liu <ltltmine@gmail.com>
2023-08-20 20:57:41 -0500

    first CI

---
+++ pipeline/etl_pipeline.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+
+class ETLPipeline:
+    def __init__(self, spark_session):
+        self.spark = spark_session
+
+    def extract(self, input_path):
+        return self.spark.read.csv(input_path, header=True, inferSchema=True)
+
+    def transform(self, data):
+        transformed_data = data.select("Name", "Age").filter(data.Age > 18)
+        return transformed_data
+
+    def load(self, transformed_data, output_path):
+        transformed_data.write.mode("overwrite").csv(output_path, header=True)
+
+def main():
+    spark = SparkSession.builder.appName("ETLPipeline").getOrCreate()
+    etl_pipeline = ETLPipeline(spark)
+
+    input_path = "input_data.csv"
+    output_path = "output_data"
+
+    data = etl_pipeline.extract(input_path)
+    transformed_data = etl_pipeline.transform(data)
+    etl_pipeline.load(transformed_data, output_path)
+
+if __name__ == "__main__":
+    main()
---
+++ test/test_etl_pipeline.py
@@ -0,0 +1,31 @@
+import pytest
+from pyspark.sql import SparkSession
+from pipeline.etl_pipeline import ETLPipeline
+
+@pytest.fixture(scope="session")
+def spark_session():
+    spark = SparkSession.builder.master("local[2]").appName("TestSession").getOrCreate()
+    yield spark
+    spark.stop()
+
+@pytest.fixture
+def etl_pipeline(spark_session):
+    return ETLPipeline(spark_session)
+
+def test_extract(etl_pipeline):
+    input_path = "sample_data.csv"  # Provide a sample CSV file for testing
+    data = etl_pipeline.extract(input_path)
+    assert data.count() > 0
+
+def test_transform(etl_pipeline):
+    # Sample data for testing transformation
+    sample_data = [("Alice", 25), ("Bob", 17), ("Charlie", 30)]
+    schema = ["Name", "Age"]
+    input_df = etl_pipeline.spark.createDataFrame(sample_data, schema=schema)
+
+    transformed_data = etl_pipeline.transform(input_df)
+    assert transformed_data.count() == 2  # Two people are older than 18
+
+# Add more tests for other components and scenarios
+
+# Run the tests: pytest test_etl_pipeline.py
